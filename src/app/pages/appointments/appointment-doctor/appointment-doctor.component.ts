6415ecea07939e705a68719b9180e8fa8a8035e0
Alejandro-Huaman <U201910144@upc.edu.pe>
2022-09-08 01:18:46 -0500

    fourth commit HU015-HU017 - HU018

--- src/app/pages/appointments/appointment-doctor/appointment-doctor.component.ts
+++ src/app/pages/appointments/appointment-doctor/appointment-doctor.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface Appointment {
+  dni: string;
+  patient: string;
+  date: string;
+  status: string;
+}
 
 @Component({
   selector: 'app-appointment-doctor',
@@ -6,14 +15,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appointment-doctor.component.css']
 })
 export class AppointmentDoctorComponent implements OnInit {
-  
+  listappointment:Appointment[] = [];
+  appointmenttest:Appointment = {dni: "123456789",patient:"paciente 1",date:"12/02/2022",status: "en curso"};
+  appointmenttest2:Appointment = {dni: "987456123",patient:"paciente 2",date:"12/02/2021",status: "cancelada"};  
   selecteddate !: Date;
   patient!:number;
   patients:string[] = ["Alayo Zavaleta, Alessandro Fabián","Almonacid Garrido, Viviana", "Benavides Castillo, Daniela"] 
+  
+  displayedColumns: string[] = ['dni', 'patient', 'date', 'status','button'];
+  dataSource = new MatTableDataSource<any>();
+  
+  @ViewChild(MatPaginator) paginator!:MatPaginator;
 
-  constructor() { }
-
+  constructor() {}
+  
   ngOnInit() {
+    console.log(this.appointmenttest)
+    console.log(this.listappointment)
+    this.listappointment.push(this.appointmenttest);
+    this.listappointment.push(this.appointmenttest2);  
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.data = this.listappointment;
+      
   }
 
 }
