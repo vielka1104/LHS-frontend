2645432d701ea06e01dceb3ff1a9d0af8c251862
giusencito <66499088+giusencito@users.noreply.github.com>
2022-09-13 18:41:28 -0500

    services ehr implement

--- src/app/services/ehr/patient-treatment.service.ts
+++ src/app/services/ehr/patient-treatment.service.ts
@@ -1,10 +1,95 @@
+import { PatientTreatmentResource } from './../../models/patient-treatment/PatientTreatmentResource';
 import { Injectable } from '@angular/core';
-
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {catchError, retry} from "rxjs/operators";
+import { CreatePatientTreatmentResource } from 'src/app/models/patient-treatment/CreatePatientTreatmentResource';
 @Injectable({
   providedIn: 'root'
 })
 export class PatientTreatmentService {
 
-constructor() { }
+  basePath =""
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  }
+constructor(private http: HttpClient) { }
+handleError(error: HttpErrorResponse) {
+  if (error.error instanceof ErrorEvent) {
+    console.log(`An error occurred: ${error.error.message} `);
+  }
+  else {
+    console.error(
+      `Backend returned code ${error.status}, body was: ${error.error}`
+    );
+  }
+
+  return throwError('Something happened with request, please try again later');
+}
+
+getAllPatientTreatments(): Observable<PatientTreatmentResource>{
+  return this.http.get<PatientTreatmentResource>(`${this.basePath}/patient-treatments`, this.httpOptions)
+  .pipe(
+    retry(2),
+    catchError(this.handleError));
+
+}
+getPatientTreatmentById(patientTreatmentId:number): Observable<PatientTreatmentResource>{
+  return this.http.get<PatientTreatmentResource>(`${this.basePath}/patient-treatments/${patientTreatmentId}`, this.httpOptions)
+  .pipe(
+    retry(2),
+    catchError(this.handleError));
+}
+getPatientTreatmentByPatientId(patientId:number){
+
+  return this.http.get<PatientTreatmentResource>(`${this.basePath}/patient-treatments/patient/${patientId}`, this.httpOptions)
+  .pipe(
+    retry(2),
+    catchError(this.handleError));
+
+
+
+}
+getPatientTreatmentByTreatmentId(treatmentId:number){
+
+  return this.http.get<PatientTreatmentResource>(`${this.basePath}/patient-treatments/treatment/${treatmentId}`, this.httpOptions)
+  .pipe(
+    retry(2),
+    catchError(this.handleError));
+
+
+
+}
+createPatientTreatment(patientId:number,treatmentId:number,medicineId:number,item:CreatePatientTreatmentResource){
+
+  return this.http.post<PatientTreatmentResource>(`${this.basePath}/patients/${patientId}/treatments/${treatmentId}/medicine/${medicineId}/patient-treatments`, JSON.stringify(item), this.httpOptions)
+  .pipe(
+    retry(2),
+    catchError(this.handleError));
+
+
+
+
+}
+
+updatePatientTreatment(patientTreatmentId:number,item:any){
+
+  return this.http.put<PatientTreatmentResource>(`${this.basePath}/patient-treatments/${patientTreatmentId}`, JSON.stringify(item), this.httpOptions)
+  .pipe(
+    retry(2),
+    catchError(this.handleError));
+
+
+}
+
+deletePatientTreatment(patientTreatmentId:number){
+  return this.http.delete(`${this.basePath}/patient-diagnosis/${patientTreatmentId}`, this.httpOptions)
+  .pipe(
+    retry(2),
+    catchError(this.handleError));
+
+}
 
 }
--- src/app/services/ehr/treatment.service.ts
+++ src/app/services/ehr/treatment.service.ts
@@ -1,10 +1,117 @@
+import { CreateTreatmentResource } from './../../models/treatment/CreateTreatmentResource';
+import { TreatmentResource } from './../../models/treatment/TreatmentResource';
 import { Injectable } from '@angular/core';
-
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {catchError, retry} from "rxjs/operators";
 @Injectable({
   providedIn: 'root'
 })
 export class TreatmentService {
 
-constructor() { }
+  basePath =""
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  }
+constructor(private http: HttpClient) { }
+handleError(error: HttpErrorResponse) {
+  if (error.error instanceof ErrorEvent) {
+    console.log(`An error occurred: ${error.error.message} `);
+  }
+  else {
+    console.error(
+      `Backend returned code ${error.status}, body was: ${error.error}`
+    );
+  }
+
+  return throwError('Something happened with request, please try again later');
+}
+
+getAllTreatments(): Observable<TreatmentResource>{
+  return this.http.get<TreatmentResource>(`${this.basePath}`, this.httpOptions)
+  .pipe(
+    retry(2),
+    catchError(this.handleError));
+
+}
+
+
+getTreatmentById(treatmentId:number): Observable<TreatmentResource>{
+
+
+  return this.http.get<TreatmentResource>(`${this.basePath}/${treatmentId}`, this.httpOptions)
+  .pipe(
+    retry(2),
+    catchError(this.handleError));
+
+
+}
+
+getTreatmentByUserName(name:string): Observable<TreatmentResource>{
+  return this.http.get<TreatmentResource>(`${this.basePath}/name/${name}`, this.httpOptions)
+  .pipe(
+    retry(2),
+    catchError(this.handleError));
+
+}
+
+
+
+createTreatment(item:CreateTreatmentResource){
+
+  return this.http.post<TreatmentResource>(`${this.basePath}`, JSON.stringify(item), this.httpOptions)
+  .pipe(
+    retry(2),
+    catchError(this.handleError));
+}
+
+updateTreatment(treatmentId:number,item:any){
+
+  return this.http.put<TreatmentResource>(`${this.basePath}/${treatmentId}`, JSON.stringify(item), this.httpOptions)
+  .pipe(
+    retry(2),
+    catchError(this.handleError));
+
+
+}
+
+deleteTreatment(treatmentId:number){
+  return this.http.delete(`${this.basePath}/${treatmentId}`, this.httpOptions)
+  .pipe(
+    retry(2),
+    catchError(this.handleError));
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
